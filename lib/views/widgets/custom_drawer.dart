50a83c64b5dbc56eab3d1bd0cacd305119676f3f
Nader <ns1568@fayoum.edu.eg>
2024-01-06 01:27:21 +0200

    edit custom drawer

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:responsive_dashboard/models/drawer_item.dart';
+import 'package:responsive_dashboard/models/user_info.dart';
+import 'package:responsive_dashboard/utils/app_images.dart';
+import 'package:responsive_dashboard/views/widgets/custom_drawer_list.dart';
+import 'package:responsive_dashboard/views/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
-
+  static List<DrawerItemModel> items = [
+    DrawerItemModel(image: Assets.imagesDashboard, title: 'Dashboard'),
+    DrawerItemModel(image: Assets.imagesMyTransation, title: 'My Transaction'),
+    DrawerItemModel(image: Assets.imagesStatistics, title: 'Statistics'),
+    DrawerItemModel(image: Assets.imagesWalletAccount, title: 'Wallet Account'),
+    DrawerItemModel(image: Assets.imagesMyInvestments, title: 'My Investments')
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
+      child: Column(
+        children: [
+          Card(
+            color: const Color(0xffFAFAFA),
+            child: UserInfoListTile(
+                userInfo: UserInfoModel(
+                    image: Assets.imagesAvatar3,
+                    title: "Lekan Okeowo",
+                    subTitle: "demo@gmail.com")),
+          ),
+          const Gap(8),
+          DrawerListItem(items: items)
+        ],
+      ),
     );
   }
 }
