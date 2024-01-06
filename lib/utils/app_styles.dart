ca3a36764741993666245678a7599181f19ef32f
Nader <ns1568@fayoum.edu.eg>
2024-01-06 10:28:47 +0200

    refactor code

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -1,50 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/utils/app_colors.dart';
 
 abstract class AppStyles {
   static const TextStyle styleRegular12 = TextStyle(
-    color: Color(0xFFAAAAAA),
+    color: AppColors.greyColor,
     fontSize: 12,
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w400,
   );
   static const TextStyle styleRegular14 = TextStyle(
-    color: Color(0xFFAAAAAA),
+    color: AppColors.greyColor,
     fontSize: 14,
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w400,
   );
   static const TextStyle styleRegular16 = TextStyle(
-    color: Color(0xFF064060),
+    color: AppColors.darkBlueColor,
     fontSize: 16,
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w400,
   );
   static const TextStyle styleMedium16 = TextStyle(
-    color: Color(0xFF064061),
+    color: AppColors.darkBlueColor,
     fontSize: 16,
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w500,
   );
   static const TextStyle styleSemiBold16 = TextStyle(
-    color: Color(0xFF064061),
+    color: AppColors.darkBlueColor,
     fontSize: 16,
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w600,
   );
+  static const TextStyle styleBold16 = TextStyle(
+    color: AppColors.blueColor,
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w700,
+  );
   static const TextStyle styleSemiBold18 = TextStyle(
-    color: Color(0xFF4EB7F2),
+    color: AppColors.blueColor,
     fontSize: 18,
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w600,
   );
   static const TextStyle styleSemiBold20 = TextStyle(
-    color: Color(0xFF064061),
+    color: AppColors.darkBlueColor,
     fontSize: 20,
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w600,
   );
   static const TextStyle styleSemiBold24 = TextStyle(
-    color: Color(0xFF4EB7F2),
+    color: AppColors.blueColor,
     fontSize: 24,
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w600,
