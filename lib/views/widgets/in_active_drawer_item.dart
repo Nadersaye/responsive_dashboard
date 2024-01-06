0145145a0e291666b802899ac2ac036c36f1a0cd
Nader <ns1568@fayoum.edu.eg>
2024-01-06 10:26:04 +0200

    add in active drawer item

---
+++ lib/views/widgets/in_active_drawer_item.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../models/drawer_item.dart';
+import '../../utils/app_styles.dart';
+
+class IntActiveDrawerItem extends StatelessWidget {
+  const IntActiveDrawerItem({super.key, required this.drawerItem});
+  final DrawerItemModel drawerItem;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: SvgPicture.asset(drawerItem.image),
+        title: Text(
+          drawerItem.title,
+          style: AppStyles.styleRegular16,
+        ));
+  }
+}
