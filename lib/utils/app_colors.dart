cbf90687fcd7e7a4b2599bc2e2ddd16840fc09e8
Nader <ns1568@fayoum.edu.eg>
2024-01-06 10:24:47 +0200

    add app colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color darkBlueColor = Color(0xff064061);
+  static const Color blueColor = Color(0xff4EB7F2);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color mediumGreyColor = Color(0xffFAFAFA);
+  static const Color greyColor = Color(0xffAAAAAA);
+  static const Color lightGreyColor = Color(0xffF1F1F1);
+  static const Color mainBackgroundColor = Color(0xFFF7F9FA);
+}
