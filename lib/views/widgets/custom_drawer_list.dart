ea40e60e18bb426ce346cfb5b0ea6d3807e90177
Nader <ns1568@fayoum.edu.eg>
2024-01-06 10:29:51 +0200

    edit drawer list to enable toggling between items

--- lib/views/widgets/custom_drawer_list.dart
+++ lib/views/widgets/custom_drawer_list.dart
@@ -2,19 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/models/drawer_item.dart';
 import 'package:responsive_dashboard/views/widgets/drawer_item.dart';
 
-class DrawerListItem extends StatelessWidget {
-  const DrawerListItem({super.key, required this.items});
-  final List<DrawerItemModel> items;
+import '../../utils/app_images.dart';
+
+class DrawerListItem extends StatefulWidget {
+  const DrawerListItem({super.key});
+  @override
+  State<DrawerListItem> createState() => _DrawerListItemState();
+}
+
+class _DrawerListItemState extends State<DrawerListItem> {
+  List<DrawerItemModel> items = [
+    DrawerItemModel(image: Assets.imagesDashboard, title: 'Dashboard'),
+    DrawerItemModel(image: Assets.imagesMyTransation, title: 'My Transaction'),
+    DrawerItemModel(image: Assets.imagesStatistics, title: 'Statistics'),
+    DrawerItemModel(image: Assets.imagesWalletAccount, title: 'Wallet Account'),
+    DrawerItemModel(image: Assets.imagesMyInvestments, title: 'My Investments')
+  ];
+  int? activeIndex;
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        physics: const NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
+    return SliverList.builder(
         itemCount: items.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.only(top: 15),
-            child: DrawerItem(drawerItem: items[index]),
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                if (activeIndex != index) {
+                  activeIndex = index;
+                }
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: DrawerItem(
+                drawerItem: items[index],
+                isActive: activeIndex == index,
+              ),
+            ),
           );
         });
   }
