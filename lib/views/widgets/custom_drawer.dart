649ab74ea61ceab80e7868d3f72b04123878a2f6
Nader <ns1568@fayoum.edu.eg>
2024-01-06 10:29:12 +0200

    edit custom drawer

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -2,35 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:responsive_dashboard/models/drawer_item.dart';
 import 'package:responsive_dashboard/models/user_info.dart';
+import 'package:responsive_dashboard/utils/app_colors.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
 import 'package:responsive_dashboard/views/widgets/custom_drawer_list.dart';
+import 'package:responsive_dashboard/views/widgets/in_active_drawer_item.dart';
 import 'package:responsive_dashboard/views/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
-  static List<DrawerItemModel> items = [
-    DrawerItemModel(image: Assets.imagesDashboard, title: 'Dashboard'),
-    DrawerItemModel(image: Assets.imagesMyTransation, title: 'My Transaction'),
-    DrawerItemModel(image: Assets.imagesStatistics, title: 'Statistics'),
-    DrawerItemModel(image: Assets.imagesWalletAccount, title: 'Wallet Account'),
-    DrawerItemModel(image: Assets.imagesMyInvestments, title: 'My Investments')
-  ];
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Card(
-            color: const Color(0xffFAFAFA),
-            child: UserInfoListTile(
-                userInfo: UserInfoModel(
-                    image: Assets.imagesAvatar3,
-                    title: "Lekan Okeowo",
-                    subTitle: "demo@gmail.com")),
+      color: AppColors.whiteColor,
+      padding: const EdgeInsets.fromLTRB(28, 0, 20, 48),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Card(
+              color: AppColors.mediumGreyColor,
+              child: UserInfoListTile(
+                  userInfo: UserInfoModel(
+                      image: Assets.imagesAvatar3,
+                      title: "Lekan Okeowo",
+                      subTitle: "demo@gmail.com")),
+            ),
           ),
-          const Gap(8),
-          DrawerListItem(items: items)
+          const SliverGap(8),
+          const DrawerListItem(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                const Expanded(
+                    child: SizedBox(
+                  height: 20,
+                )),
+                IntActiveDrawerItem(
+                    drawerItem: DrawerItemModel(
+                        image: Assets.imagesSetting, title: 'Setting system')),
+                IntActiveDrawerItem(
+                    drawerItem: DrawerItemModel(
+                        image: Assets.imagesLogout, title: 'Logout account')),
+              ],
+            ),
+          )
         ],
       ),
     );
