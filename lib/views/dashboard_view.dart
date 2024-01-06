3d06bbfbd701b238e7b317d6b8cc8a180a3897de
Nader <ns1568@fayoum.edu.eg>
2024-01-06 10:25:15 +0200

    add background color

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/utils/app_colors.dart';
 import 'package:responsive_dashboard/views/widgets/adaptive_layout.dart';
 
 import 'widgets/dashboard_desktop_layout.dart';
@@ -9,6 +10,7 @@ class DashboardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.mainBackgroundColor,
       body: AdaptiveLayout(
           mobileLayout: (context) => const SizedBox(),
           tabletLayout: (context) => const SizedBox(),
