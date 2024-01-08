4cf12f6023f11973a3ac5e120c0896c8635008e0
Nader <ns1568@fayoum.edu.eg>
2024-01-08 05:29:27 +0200

    add expenses invoice

---
+++ lib/views/widgets/expenses_invoice.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:responsive_dashboard/views/widgets/all_expenses.dart';
+import 'package:responsive_dashboard/views/widgets/quick_invoice_section.dart';
+
+class ExpensesInvoice extends StatelessWidget {
+  const ExpensesInvoice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [AllExpenses(), Gap(24), QuikInvoiceSection()],
+    );
+  }
+}
