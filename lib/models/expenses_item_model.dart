9bfca770bcdcd48e61d138dee21e4ac9ec0f3eee
Nader <ns1568@fayoum.edu.eg>
2024-01-08 05:23:41 +0200

    add expenses item model

---
+++ lib/models/expenses_item_model.dart
@@ -0,0 +1,10 @@
+class ExpensesItemModel {
+  final String image, title, date;
+  final double amount;
+
+  const ExpensesItemModel(
+      {required this.image,
+      required this.title,
+      required this.date,
+      required this.amount});
+}
