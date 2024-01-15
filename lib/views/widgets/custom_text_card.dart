30bd985e19e57efddb8138d697d40433c50764d4
Nader <ns1568@fayoum.edu.eg>
2024-01-15 18:37:21 +0200

    add custom text card

---
+++ lib/views/widgets/custom_text_card.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
+import 'package:responsive_dashboard/views/widgets/customer_text_field.dart';
+
+import '../../models/cutomer_text_card_model.dart';
+
+class CustomTextCard extends StatelessWidget {
+  const CustomTextCard({super.key, required this.item});
+  final CustomerTextCardModel item;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Customer name', style: AppStyles.styleMedium16),
+          const Gap(12),
+          CustomerTextField(
+            item: item,
+          )
+        ],
+      ),
+    );
+  }
+}
