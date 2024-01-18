4559dc41031cbe376163e7074ba1fc3ec57b2b39
Nader <ns1568@fayoum.edu.eg>
2024-01-18 16:30:13 +0200

    add income body

---
+++ lib/views/widgets/income_body.dart
@@ -0,0 +1,60 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import '../../models/income_model.dart';
+import '../../utils/app_colors.dart';
+import 'income_list_view.dart';
+
+class IncomeBody extends StatefulWidget {
+  const IncomeBody({super.key});
+
+  @override
+  State<IncomeBody> createState() => _IncomeBodyState();
+}
+
+class _IncomeBodyState extends State<IncomeBody> {
+  int currentIndex = -1;
+  List<IncomeModel> charts = [
+    IncomeModel(value: 40, color: AppColors.chartBlue, title: 'Design service'),
+    IncomeModel(value: 25, color: AppColors.blueColor, title: 'Design product'),
+    IncomeModel(
+        value: 22, color: AppColors.darkBlueColor, title: 'Product royalti'),
+    IncomeModel(value: 20, color: AppColors.chartgrey, title: 'Other'),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: AspectRatio(aspectRatio: 1, child: getChartsData())),
+        Expanded(
+          flex: 2,
+          child: IncomeListView(
+            incomes: charts,
+          ),
+        )
+      ],
+    );
+  }
+
+  PieChart getChartsData() {
+    return PieChart(PieChartData(
+      pieTouchData: PieTouchData(
+        enabled: true,
+        touchCallback: (flTouchEvent, pieTouchResponse) {
+          currentIndex =
+              pieTouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
+          setState(() {});
+        },
+      ),
+      sectionsSpace: 0,
+      sections: List<PieChartSectionData>.generate(
+        4,
+        (index) => PieChartSectionData(
+          showTitle: false,
+          value: charts[index].value,
+          radius: currentIndex == index ? 60 : 50,
+          color: charts[index].color,
+        ),
+      ),
+    ));
+  }
+}
