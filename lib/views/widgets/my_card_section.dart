474a027f731e6211bef8b1fd691bb061f3316299
Nader <ns1568@fayoum.edu.eg>
2024-01-18 16:59:51 +0200

    add my card section

---
+++ lib/views/widgets/my_card_section.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
+import 'package:responsive_dashboard/views/widgets/custom_card_page_view.dart';
+import 'package:responsive_dashboard/views/widgets/dot_indicator_row.dart';
+
+class MyCardSection extends StatefulWidget {
+  const MyCardSection({super.key});
+  @override
+  State<MyCardSection> createState() => _MyCardSectionState();
+}
+
+class _MyCardSectionState extends State<MyCardSection> {
+  late int currentIndex;
+  late PageController pageController;
+  @override
+  void initState() {
+    currentIndex = 0;
+    pageController = PageController();
+    pageController.addListener(() {
+      currentIndex = pageController.page!.round();
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('My card', style: AppStyles.styleSemiBold20),
+        const Gap(20),
+        CustomCardPageView(
+          pageController: pageController,
+        ),
+        const Gap(20),
+        DotIndicatorRow(currentIndex: currentIndex)
+      ],
+    );
+  }
+}
