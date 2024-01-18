80502d152e4ea0c7ebfa075b7e5b1ebd5c4a779b
Nader <ns1568@fayoum.edu.eg>
2024-01-18 17:04:34 +0200

    add some new colors

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -8,4 +8,10 @@ abstract class AppColors {
   static const Color greyColor = Color(0xffAAAAAA);
   static const Color lightGreyColor = Color(0xffF1F1F1);
   static const Color mainBackgroundColor = Color(0xFFF7F9FA);
+  static const Color dotGrey = Color(0xFFE7E7E7);
+  static const Color amoutRed = Color(0xFFF3735E);
+  static const Color amoutGreen = Color(0xFF7CD87A);
+  static const Color chartBlue = Color(0xFF208BC7);
+  static const Color chartgrey = Color(0xFFE2DECD);
+  static const Color incomeBlue = Color(0xFF208CC8);
 }
