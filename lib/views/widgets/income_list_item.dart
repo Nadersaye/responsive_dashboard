c80d66a016be941d9403e5040ef6f3dc3543993b
Nader <ns1568@fayoum.edu.eg>
2024-01-18 16:30:54 +0200

    add income list item

---
+++ lib/views/widgets/income_list_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/models/income_model.dart';
+import 'package:responsive_dashboard/utils/app_colors.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
+
+class IncomeListItem extends StatelessWidget {
+  const IncomeListItem({super.key, required this.income});
+  final IncomeModel income;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 6,
+        foregroundColor: income.color,
+      ),
+      title: Text(income.title, style: AppStyles.styleSemiBold16),
+      trailing: Text(
+        '${income.value}%',
+        style: AppStyles.styleMedium16.copyWith(color: AppColors.incomeBlue),
+      ),
+    );
+  }
+}
